22c10fab7b159731535815c61a040cdb70c40353
Harsh Srivastav <harshsrivastav09011@gmail.com>
2024-06-25 11:19:16 +0530

    add all file

---
+++ force-app/main/default/lwc/dogApi/dogApi.js
@@ -0,0 +1,27 @@
+import { LightningElement } from 'lwc';
+import getDogPicture from '@salesforce/apex/DogPictureCallOut.getDogPicture';
+export default class DogApi extends LightningElement {
+
+
+
+imageReady = false ;
+
+loadingSpinner = false;
+
+pictureUrl;
+
+handleClick(){
+    this.loadingSpinner =true;
+    this.imageReady = false;
+
+    getDogPicture({}).then(result=> {
+        this.pictureUrl = JSON.parse(result).message;
+        this.loadingSpinner = false;
+        this.imageReady = true;
+
+    });
+
+    
+}
+
+}
